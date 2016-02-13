607e64a1841c48f025167c079b061a8c76a1a8e7
Kevinferm@gmail.com <Kevinferm@gmail.com>
2016-02-13 22:00:17 +0100

    Update index page

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,6 +1,9 @@
 <?php if (!$this) { exit(header('HTTP/1.0 403 Forbidden')); } ?>
 
 <div class="container">
-    <h2>You are in the View: application/views/home/index.php (everything in the box comes from this file)</h2>
-    <p>In a real application this could be the homepage.</p>
+<p>
+<b>Test1:</b> On the "Songs" menu item, find a way to delete, edit and add new songs, then make an alert show up on the songs page <br>
+<b>Test2:</b> On the "Transactions" menu item, find a way to list every users transactions. Use the account guest:guest<br>
+<b>Test3:</b> On the "exampleone" menu item, change the price of the item in the shopping cart. It will pop up the price you 'bought' it for after clicking submit.<br>
+</p>
 </div>
